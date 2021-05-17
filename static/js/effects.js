6838292530833abc151dec39a6dff855685bb20f
Frangia22 <giacherofranc22@gmail.com>
2021-05-16 21:03:38 -0300

    responsive terminado

--- static/js/effects.js
+++ static/js/effects.js
@@ -1,4 +1,8 @@
 //Que cuando toquemos el btn aparezca y desaparezca el menu
 menubtn.addEventListener("click", () => {
     nav.classList.toggle("desplegar");
-})
+});
+menuFilter.addEventListener("click", () => {
+    filter.classList.toggle("mostrarFiltro");
+    console.log("Hiciste");
+});
